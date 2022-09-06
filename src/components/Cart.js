74555757da01ebff1fa8cdc401d10585260cfdb6
Hamidou.Sow <hamidou.sow@hotmail.fr>
2022-09-06 12:09:08 +0200

    adding, images, update css, useEffect

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,16 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import '../styles/Cart.css'
 
-function Cart ({ cart, updateCart }) {
-	const monsteraPrice = 8
+function Cart({ cart, updateCart }) {
 	const [isOpen, setIsOpen] = useState(true)
+	const total = cart.reduce(
+		(acc, plantType) => acc + plantType.amount * plantType.price,
+		0
+	)
+
+	useEffect(() => {
+		document.title = `LMJ: ${total}€ à payer`
+	}, [total])
 
 	return isOpen ? (
 		<div className='lmj-cart'>
@@ -13,13 +20,22 @@ function Cart ({ cart, updateCart }) {
 			>
 				Fermer
 			</button>
-			<h2>Panier</h2>
-			<div>Monstera : {monsteraPrice}€</div>			
-			<h3>Total : {monsteraPrice * cart}€</h3>
-            <button onClick={() => updateCart(0)}> 
-                Vider le panier 
-            </button>
-            
+			{cart.length > 0 ? (
+				<div>
+					<h2>Panier</h2>
+					<ul>
+						{cart.map(({ name, price, amount }, index) => (
+							<div key={`${name}-${index}`}>
+								{name} {price}€ x {amount}
+							</div>
+						))}
+					</ul>
+					<h3>Total :{total}€</h3>
+					<button onClick={() => updateCart([])}>Vider le panier</button>
+				</div>
+			) : (
+				<div>Votre panier est vide</div>
+			)}
 		</div>
 	) : (
 		<div className='lmj-cart-closed'>
