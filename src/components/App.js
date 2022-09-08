bea4e12f4dadeec08d2ea56818ff3c38cb2c3e72
Hamidou.Sow <hamidou.sow@hotmail.fr>
2022-09-08 10:59:27 +0200

    useEffect hook

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,30 @@ import Cart from './Cart';
 import Footer from './Footer';
 import ShoppingList from './ShoppingList';
 import logo from '../assets/logo.png'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const [cart, updateCart] = useState([])
+
+    const savedCart = localStorage.getItem('cart')
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
   
-  return (
-      <div>
-          <Banner>
-              <img src={logo} alt='La maison jungle' className='lmj-logo' />
-              <h1 className='lmj-title'>La maison jungle</h1>
-          </Banner>
-          <div className='lmj-layout-inner'>
-              <Cart cart={cart} updateCart={updateCart} />
-              <ShoppingList cart={cart} updateCart={updateCart} />
-          </div>
-          <Footer />
-      </div>
-  )
+    return (
+        <div>
+            <Banner>
+                <img src={logo} alt='La maison jungle' className='lmj-logo' />
+                <h1 className='lmj-title'>La maison jungle</h1>
+            </Banner>
+            <div className='lmj-layout-inner'>
+                <Cart cart={cart} updateCart={updateCart} />
+                <ShoppingList cart={cart} updateCart={updateCart} />
+            </div>
+            <Footer />
+        </div>
+    )
 }
 
 export default App
